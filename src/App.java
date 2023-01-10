d6fecd1df18e1d8f3facfc3ac8c17a1e64953f25
Lucas Lafèré <laferelucas@gmail.com>
2023-01-10 11:40:21 -0300

    fixed not multiplying up to 10

--- src/App.java
+++ src/App.java
@@ -2,9 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int[] tabelaQuantidade = new int[10];
-        int x = 1;
+        int x = 4;
         System.out.println("Tabela de multiplicação de " + x + ", de 1 a 10:");
-        for (int i = 0; i < tabelaQuantidade.length; i++) {
+        for (int i = 0; i <= tabelaQuantidade.length; i++) {
         System.out.println(x * i);
         }
         
