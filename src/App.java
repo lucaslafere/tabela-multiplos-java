8aaa2e0ffa6e5cd9945cb5d4fcf9f6ef597b66a7
Lucas Lafèré <laferelucas@gmail.com>
2023-01-10 11:45:01 -0300

    added user input

--- src/App.java
+++ src/App.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.println("Informe o número a obter a tabela de múltiplos");
+        int numeroMultiplicado = scanner.nextInt();
+
 
         int[] tabelaQuantidade = new int[10];
         int x = 4;
-        System.out.println("Tabela de multiplicação de " + x + ", de 1 a 10:");
+        System.out.println("\nTabela de multiplicação de " + numeroMultiplicado + ", de 1 a 10:");
         for (int i = 0; i <= tabelaQuantidade.length; i++) {
         System.out.println(x * i);
         }
