0b128ef7db29fcc0ffa681199de97495ca5311cd
Lucas Lafèré <laferelucas@gmail.com>
2023-01-10 11:38:14 -0300

    added numbers printing

--- src/App.java
+++ src/App.java
@@ -2,13 +2,11 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int[] tabelaQuantidade = new int[10];
-        
+        int x = 0;
+        System.out.println("Tabela de multiplicação de " + x + ", de 1 a 10:");
         for (int i = 0; i < tabelaQuantidade.length; i++) {
-            
+        System.out.println(tabelaQuantidade[i]);
         }
-
-
-
-        System.out.println("Tabela de multiplicação de " + x + ", de 1 a 10:" + tabela);
+        
     }
 }
